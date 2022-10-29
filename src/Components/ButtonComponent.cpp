e770fe4552f45628518213498430df77b48a4b17
Ma-tes <msestaubr@gmail.com>
2022-10-29 20:40:17 +0200

    Initial release

---
+++ src/Components/ButtonComponent.cpp
@@ -0,0 +1,40 @@
+#include "ButtonComponent.h"
+#include <Arduino.h>
+#include "Component.h"
+
+
+
+namespace Components
+{
+    #define BUTTON_DELAY 50
+
+    ButtonComponent::ButtonComponent(ButtonAction buttonAction, byte pin) : Component(pin, INPUT_PULLUP)
+    {
+        action = buttonAction;
+    }
+
+    void ButtonComponent::OnLoad()
+    {
+    }
+
+    void ButtonComponent::OnUpdate(long difference)
+    {
+        long currentDifference = difference - lastDebounce;
+        if(difference > BUTTON_DELAY)
+        {
+            auto currentState = IsPressed();
+            if(currentState != lastButtonState)
+            {
+                lastButtonState = currentState;
+                lastDebounce = currentDifference;
+            }
+            else
+                action(currentState);
+        }
+    }
+
+    bool ButtonComponent::IsPressed()
+    {
+        return digitalRead(Pin) == HIGH;
+    }
+}
