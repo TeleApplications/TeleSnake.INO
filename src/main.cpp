d791807cd9b9f2dc7ab1d8470352e97012af8c94
Ma-tes <msestaubr@gmail.com>
2022-10-30 12:15:33 +0100

    Created small render manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,40 @@
 #include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
 #include "Components\ButtonComponent.h"
-
+#include "RenderComponent\RenderManager.h"
+#include "Snake.h"
 
 #define ONE_INTERATION 500
-Components::Component *currentComponents[1];
+
+
+RenderComponent::IRenderable *renderComponents[1];
+RenderComponent::RenderManager *renderManager;
+
 long lastMilliseconds;
+
+RenderComponent::DisplayInformation displayInformation;
+Components::Component *currentComponents[1];
 
 void getTestButtonAction(bool state);
 
-
 void setup()
 {
-    Serial.begin(9400);
+    Serial.begin(9600);
     Serial.print("Setup is already done");
 
     pinMode(13, OUTPUT);
+
+    renderComponents[0] = new src::Snake();
     currentComponents[0] = new Components::ButtonComponent(getTestButtonAction, 7);
 
+    displayInformation.width = 128;
+    displayInformation.height = 64;
+    displayInformation.address = 0x3C;
+
+    renderManager = new RenderComponent::RenderManager(*renderComponents, displayInformation);
     for(int i = 0; i < 1; i++)
     {
         auto& currentComponent = *currentComponents[i];
@@ -26,8 +44,8 @@ void setup()
 
 void loop()
 { 
+    (*renderManager).StartRender();
     long difference = millis() - lastMilliseconds;
-    Serial.write(difference);
 
     if(difference > ONE_INTERATION)
         lastMilliseconds = millis();
