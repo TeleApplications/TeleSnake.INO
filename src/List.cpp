ac6bb14926d60735cbb178aee85a06618d30d2e0
Ma-tes <msestaubr@gmail.com>
2022-10-31 06:39:46 +0100

    Created uncomplete version of collection system

---
+++ src/List.cpp
@@ -0,0 +1,19 @@
+#include "List.h"
+
+namespace src
+{
+    template <class T>
+    List<T>::List(int bufferSize = 32)
+    {
+    }
+
+    template <class T>
+    void List<T>::Add(T value)
+    {
+    }
+
+    template <class T>
+    void List<T>::Remove(T value)
+    {
+    }
+};
