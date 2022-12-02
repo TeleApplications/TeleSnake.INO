0ce043a626ce43380aad479ed79985b22088b389
Ma-tes <msestaubr@gmail.com>
2022-12-02 22:02:52 +0100

    Added new button layout

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ RenderComponent::RenderManager *renderManager;
 long lastMilliseconds;
 
 RenderComponent::DisplayInformation displayInformation;
-Components::Component *currentComponents[2];
+Components::Component *currentComponents[4];
 src::Snake *currentSnake;
 
 void getTestButtonAction(bool state, Components::ButtonComponent* component);
@@ -25,25 +25,24 @@ void getTestButtonAction(bool state, Components::ButtonComponent* component);
 void setup()
 {
     Serial.begin(9600);
-    //Serial.print("Setup is already done");
-
     pinMode(13, OUTPUT);
 
-
+    renderComponents[0] = currentSnake;
     currentSnake = new src::Snake();
     currentSnake->Position.X = 50;
     currentSnake->Position.Y = 50;
 
-    renderComponents[0] = currentSnake;
-    currentComponents[0] = new Components::ButtonComponent(getTestButtonAction, 11);
-    currentComponents[1] = new Components::ButtonComponent(getTestButtonAction, 10);
+    currentComponents[0] = new Components::ButtonComponent(getTestButtonAction, 4);
+    currentComponents[1] = new Components::ButtonComponent(getTestButtonAction, 5);
+    currentComponents[2] = new Components::ButtonComponent(getTestButtonAction, 6);
+    currentComponents[3] = new Components::ButtonComponent(getTestButtonAction, 8);
 
     displayInformation.width = 128;
     displayInformation.height = 64;
     displayInformation.address = 0x3C;
 
     renderManager = new RenderComponent::RenderManager(*renderComponents, displayInformation);
-    for(int i = 0; i < 2; i++)
+    for(int i = 0; i < 3; i++)
     {
         auto& currentComponent = *currentComponents[i];
         currentComponent.OnLoad();
@@ -58,31 +57,41 @@ void loop()
     if(difference > ONE_INTERATION)
         lastMilliseconds = millis();
 
-    for(int i = 0; i < 2; i++)
+    for(int i = 0; i < 3; i++)
     {
         auto& currentComponent = *currentComponents[i];
         currentComponent.OnUpdate(difference);
     }
+        digitalWrite(13, LOW);
 } 
 
 void getTestButtonAction(bool state, Components::ButtonComponent* component)
 {
-    if(component->Pin == 10 && state)
+    if(component->Pin == 4 && state)
     {
-        digitalWrite(13, LOW);
-        currentSnake->Direction = src::Vector2::Down();
+        digitalWrite(13, HIGH);
+        currentSnake->Direction = src::Vector2::Left();
+        Serial.println("Left");
     }
-    if(component->Pin == 11 && state)
+
+    if(component->Pin == 5 && state)
+    {
+        digitalWrite(13, HIGH);
+        currentSnake->Direction = src::Vector2::Down();
+        Serial.println("Down");
+    }
+
+    if(component->Pin == 6 && state)
+    {
+        digitalWrite(13, HIGH);
+        currentSnake->Direction = src::Vector2::Up();
+        Serial.println("Up");
+    }
+
+    if(component->Pin == 8 && state)
     {
         digitalWrite(13, HIGH);
         currentSnake->Direction = src::Vector2::Right();
-    }
-    if(component->Pin == 9 && state)
-    {
-        currentSnake->Direction = src::Vector2::Up();
-    }
-    if(component->Pin == 8 && state)
-    {
-        currentSnake->Direction = src::Vector2::Down();
+        Serial.println("Right");
     }
 }
